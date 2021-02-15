0ae0812552031f616fceb2708f99f3d06a20a030
Abrham Tesfaye <se.abrham.tesfaye@gmail.com>
2021-02-15 21:38:39 +0300

    added the resize event handler on the clock hands

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,40 @@
 let image  = document.getElementsByTagName("img")[0]
 let minuteHand = document.querySelector("#minute")
 let hourHand = document.querySelector("#hour")
+let secondHand = document.querySelector("#second")
+let center = document.querySelector("#center")
 let imageStyle = getComputedStyle(image)
 console.log(imageStyle["width"])
 
-var x=1
-// window.onresize = (e)=>{
-//     imageStyle=getComputedStyle(image);
-//     // console.log(imageStyle["width"]);
-//     width = parseInt(imageStyle["width"])
-//     var rect = image.getBoundingClientRect();
-//     let centery = rect.top+width/2
-//     let centerx = rect.left+width/2
-//     minuteHand.style.top = `${centery}px`
-//     minuteHand.style.left = `${centerx}px`
-//     minuteHand.style.height = (width/2-width*0.2)+"px"
-//     minuteHand.style.transform = `rotate(${15*x++}deg)`
-//     minuteHand.style.transformOrigin = "top left"
 
-// }
+window.onresize = (e)=>{
+    
+placeObjects()
+}
+
 width = parseInt(imageStyle["width"])
 var rect = image.getBoundingClientRect();
 let centery = rect.top+width/2
 let centerx = rect.left+width/2
-minuteHand.style.top = `${centery}px`
-minuteHand.style.left = `${centerx}px`
 
-setInterval(
-    (e)=>{
-        
-        imageStyle=getComputedStyle(image);
-        // console.log(imageStyle["width"]);
-        width = parseInt(imageStyle["width"])
-        var rect = image.getBoundingClientRect();
-        let centery = rect.top+width/2
-        let centerx = rect.left+width/2
-        minuteHand.style.top = `${centery}px`
-        minuteHand.style.left = `${centerx}px`
-        minuteHand.style.height = (width/2-width*0.2)+"px"
-        
-        minuteHand.style.transform = `rotate(${15*x++}deg)`
-        minuteHand.style.transformOrigin = "top left"
-    }
-    ,1000
-)
+function placeObjects(){
+    imageStyle=getComputedStyle(image);
+    // console.log(imageStyle["width"]);
+    width = parseInt(imageStyle["width"])
+    var rect = image.getBoundingClientRect();
+    let centery = rect.top+width/2
+    let centerx = rect.left+width/2
+    minuteHand.style.top = `${centery}px`
+    minuteHand.style.left = `${centerx}px`
+    minuteHand.style.height = (width/2-width*0.25)+"px"
+    hourHand.style.top = `${centery}px`
+    hourHand.style.left = `${centerx}px`
+    hourHand.style.height = (width/2-width*0.3)+"px"
+    secondHand.style.top = `${centery}px`
+    secondHand.style.left = `${centerx}px`
+    secondHand.style.height = (width/2-width*0.2)+"px"
+    center.style.top = `${centery-5}px`
+    center.style.left = `${centerx-5}px`
+}
+
+placeObjects()
